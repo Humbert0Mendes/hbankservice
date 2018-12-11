6d921bb138422028d46d75837cc0263cba91f8c7
Humbert0Mendes <hmpn87@gmail.com>
2018-12-11 14:11:19 -0200

    Inicializando repositório

---
+++ src/main/java/com/project/hbankservice/hbankservice/HbankserviceApplication.java
@@ -0,0 +1,12 @@
+package com.project.hbankservice.hbankservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbankserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbankserviceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/project/hbankservice/hbankservice/HbankserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.project.hbankservice.hbankservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HbankserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
