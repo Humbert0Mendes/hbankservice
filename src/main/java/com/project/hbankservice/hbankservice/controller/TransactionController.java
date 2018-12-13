c80e2c545154cbda4e1a798750d2a3c2b05cb0a1
Humbert0Mendes <hmpn87@gmail.com>
2018-12-13 19:05:40 -0200

    Criação do Controller para realziar as transações

---
+++ src/main/java/com/project/hbankservice/hbankservice/controller/TransactionController.java
@@ -0,0 +1,12 @@
+package com.project.hbankservice.hbankservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/transaction")
+public class TransactionController {
+	
+	
+
+}
